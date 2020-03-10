4b64f78125e6bb34cea546544a4c5765372522db
MananTaneja <manantaneja007@gmail.com>
2020-03-11 01:13:34 +0530

    added progress bar + minor changes

--- webappadmin/src/pages/Actions.js
+++ webappadmin/src/pages/Actions.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import {ButtonToolbar, ButtonGroup, Button, Jumbotron, Row, Col} from 'react-bootstrap';
+import {ButtonToolbar, ButtonGroup, Button, Jumbotron, ProgressBar} from 'react-bootstrap';
 
 class Actions extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            now: 50
+        };
+        this.updateProgress = this.updateProgress.bind(this);
+    }
     
+    updateProgress() {
+        if(this.state.now <= 80) {
+           const temp = this.state.now + 20;
+           this.setState({
+               now: temp
+           });
+        }
+        else {
+            this.setState({
+                now: 0
+            });
+        }
+    }
+
     render() {
         return (
             <div className="col-md-12"> 
                 <Jumbotron>
+                    <ProgressBar animated variant="danger" now={this.state.now} />
                     <ButtonToolbar aria-label="Toolbar with Button groups" className="justify-content-between">
                         <ButtonGroup className="mr-2" aria-label="First group" size="lg">
-                        <Button style = {{padding: "3vw", margin: "1vw", width: "20vw" }} variant="success">Step 1: Allot Electivies</Button>{' '}
+                        <Button style = {{padding: "3vw", margin: "1vw", width: "20vw" }} variant="success" onClick={this.updateProgress}>Step 1: Allot Electivies</Button>{' '}
                         <Button style = {{padding: "3vw", margin: "1vw", width: "20vw" }} variant="secondary">Step 2: Generate Mailing List</Button>
                         </ButtonGroup>
                     </ButtonToolbar>
