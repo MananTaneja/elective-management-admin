5a87be2ab17f881c815682898d2ffbdfa12f6ee3
MananTaneja <manantaneja007@gmail.com>
2020-03-16 09:16:47 +0530

    added files for testing

---
+++ webappadmin/src/pages/Requests.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Transactions from './Transactions';
+import {Jumbotron, Container} from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+class Requests extends Component {
+
+}
